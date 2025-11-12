db50b823f10f8ededae135e61f4e746fc83fd713
sivsoerensen <72440899+sivsoerensen@users.noreply.github.com>
2025-11-12 09:14:38 +0000

    api OpenAI status

--- api/sivgpt.js
+++ api/sivgpt.js
@@ -42,6 +42,8 @@ export default async function handler(req, res) {
       }),
     });
 
+    console.log("OpenAI status:", r.status);
+
     const data = await r.json();
 
     // --- Allow requests from browser ---
